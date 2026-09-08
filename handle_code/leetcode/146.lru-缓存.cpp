c4a838faeec69d5f09863e3312c0ef134fd41c10
Alizen-1009 <alizenjames@gmail.com>
2026-09-08 14:14:02 +0800

    111 update mini

--- handle_code/leetcode/146.lru-缓存.cpp
+++ handle_code/leetcode/146.lru-缓存.cpp
@@ -6,28 +6,54 @@
 
  // @lc code=start
 class LRUCache {
-public:
     struct ListNode {
-        int val;
+        int key, val;
         ListNode* pre, * next;
-        ListNode(int val) {
-            val = val;
+        ListNode(int key, int val) {
+            this->key = key;
+            this->val = val;
             pre = nullptr;
             next = nullptr;
         }
     };
+    int capacity = 0;
     unordered_map<int, ListNode*> mp;
-    int num;
-    ListNode dumm1 = ListNode(-1);
-    ListNode dumm2 = ListNode(-1);
-    ListNode* head = &dumm1;
-    ListNode* end = &dumm2;
+    ListNode* head, * tail;
+    void remove(ListNode* p) {
+        p->next->pre = p->pre;
+        p->pre->next = p->next;
+    }
+
+    void addtohead(ListNode* p) {
+        head->next->pre = p;
+        p->next = head->next;
+        p->pre = head;
+        head->next = p;
+    }
+
+    void movetohead(ListNode* p) {
+        remove(p);
+        addtohead(p);
+    }
+
+    void delete_last() {
+        ListNode* p = tail->pre;
+        mp.erase(p->key);
+        remove(p);
+        delete p;
+    }
+
+public:
     LRUCache(int capacity) {
-        num = capacity;
+        this->capacity = capacity;
+        head = new ListNode(0, 0);
+        tail = new ListNode(0, 0);
+        head->next = tail;
+        tail->pre = head;
     }
 
     int get(int key) {
-        if (mp[key]) {
+        if (mp.contains(key)) {
             int val = mp[key]->val;
             movetohead(mp[key]);
             return val;
@@ -36,24 +62,17 @@ public:
     }
 
     void put(int key, int val) {
-        ListNode tmp = ListNode(val);
-        mp[key] = &tmp;
-        movetohead(mp[key]);
-        if (mp.size() > num) delete_last();
-    }
-
-    void movetohead(ListNode* p) {
-        if (p->pre->next) p->pre->next = p->next;
-        if (p->next->pre) p->next->pre = p->pre;
-        p->next = head;
-    }
-
-    void delete_last() {
-        // mp[end->pre] = nullptr;
-        end->pre = end->pre->pre;
-
-        end->pre->pre->next = end;
-
+        if (mp.contains(key)) {
+            mp[key]->val = val;
+            movetohead(mp[key]);
+        }
+        else {
+            ListNode* tmp = new ListNode(key, val);
+            mp[key] = tmp;
+            addtohead(mp[key]);
+            if (mp.size() > capacity)
+                delete_last();
+        }
     }
 };
 
